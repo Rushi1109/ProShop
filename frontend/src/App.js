c3ea68b0c645d1a42510745979bf39f2f185c6b1
Rushi_Gandhi <rushigandhi1109@gmail.com>
2023-03-10 18:42:42 +0530

    Added react-bootstrap and displayed products on Home Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
     return (
         <>
+            <Header />
+            <main className='py-3'>
+                <Container>
+                    <HomeScreen />
+                </Container>
+            </main>
+            <Footer />
         </>
     );
 }
