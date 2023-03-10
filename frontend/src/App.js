d7897c8b2eb9f91ddd76f894fca47ccfb89a82d8
Rushi_Gandhi <rushigandhi1109@gmail.com>
2023-03-10 21:48:37 +0530

    Added Single Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
     return (
         <>
-            <Header />
-            <main className='py-3'>
-                <Container>
-                    <HomeScreen />
-                </Container>
-            </main>
-            <Footer />
+            <Router>
+                <Header />
+                <main className='py-3'>
+                    <Container>
+                        <Routes>
+                            <Route exact path='/' element={<HomeScreen />} />
+                            <Route exact path='/product/:productId' element={<ProductScreen />} />
+                        </Routes>
+                    </Container>
+                </main>
+                <Footer />
+            </Router>
         </>
     );
 }
